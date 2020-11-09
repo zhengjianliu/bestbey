8c2105f83586e6e59ecc250d82a8f8dce299b6a7
jdhofmann <justindhofmann@gmail.com>
2020-11-09 09:16:27 -0500

    simple login added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 import Home from './containers/Home'
 import NavBar from './containers/NavBar';
 import Showpage from './containers/Showpage'
-import SlidePanel from './containers/SlidePanel';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
+// import SlidePanel from './containers/SlidePanel';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+// import { Navbar } from 'react-bootstrap';
 
 class App extends React.Component {
 
@@ -23,8 +23,23 @@ class App extends React.Component {
     .catch(e => console.error(e))
   }
 
+  handleUserLogin = (userObj) => {
+    // console.log(userObj)
+    fetch("http://localhost:3000/users")
+    .then(response => response.json())
+    .then(users => this.findUser(users, userObj))
+  }
+
+  findUser = (users, userObj) =>{
+    let newUser =  users.find(user => 
+      user.username === userObj.username &&
+      user.password === userObj.password
+      )
+    if (newUser) {this.setState({user: newUser})} 
+  }
+
   filteredContent = () =>{
-    if (this.state.searchterm == ""){
+    if (this.state.searchterm === ""){
       return this.state.products
     }else{
       return this.state.products.filter(product=>product.name.toUpperCase().includes(this.state.searchterm.toUpperCase()) || product.brand.toUpperCase().includes(this.state.searchterm.toUpperCase()))
@@ -44,8 +59,16 @@ class App extends React.Component {
     return (
       <Router>
         <div className="App">
-          <NavBar cart={this.state.cart} products={this.state.products} searchHandler={this.searchHandler} searchterm={this.state.searchterm}/>
-          <Route exact path="/" render={() =><Home products={this.filteredContent()} cartChangeHandler={this.cartChangeHandler}/>}/>
+          <NavBar 
+            cart={this.state.cart} 
+            products={this.state.products} 
+            searchHandler={this.searchHandler} 
+            searchterm={this.state.searchterm}
+            handleUserLogin={this.handleUserLogin}
+            />
+          <Route exact path="/" render={() =>
+            <Home    
+              products={this.filteredContent()} cartChangeHandler={this.cartChangeHandler}/>}/>
           <Route path="/showpage" render={() =><Showpage />}/>
         </div>
       </Router>
