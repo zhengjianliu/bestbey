f10d3490c93d281d65ee88732c99dd5e55ce9458
zhengjianliu <comezheng@gmail.com>
2020-12-11 12:43:18 -0500

    udate

--- src/containers/CheckoutPage.js
+++ src/containers/CheckoutPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CartItem from '../components/CartItem'
-import {Link, Redirect} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import emptycart from '../images/emptycart.png'
 
 
